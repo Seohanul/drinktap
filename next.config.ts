9a7bb94cff22b9adf8b66722230d0e4551d1722b
Seohanul <xjfqlddl13@naver.com>
2026-04-20 00:26:33 +0900

    Covert to static export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+};
 
 export default withNextIntl(nextConfig);
